6407fb625e4d9e32d3cae730d0fd771b65d47529
James Snell <Vulpes3.1415@yahoo.com>
2019-09-06 11:56:20 -0600

    Expanded todo steps. Wrote in example pieces of code to use on project

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+// computer makes choice out of word out of bank -->
+var rocks = ["Gold", "Opal", "Diamond", "Jade", "Quartz", "Sapphire"]
+// math.random for array of possible words in bank 
+function word (math.random(rocks.length-1))
+
+// computer displays underscores for word _ _ _ _ 
+
+
+// computer senses what character is being pressed by human displays error message if alphabet character not choice
+
+
+// displays wrong characters
+// alert
+// displays right letter in  _ _ _ place
+// conditional loop ==== on key press 
+
+// plays song or sound when game over
+
+// dom model
+var exampleDiv = document.getElementById("example");
+exampleDiv.textContent = "rocks"
+
